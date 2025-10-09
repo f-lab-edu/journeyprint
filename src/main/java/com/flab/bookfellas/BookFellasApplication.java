3ea7a2e6d7313f733c2e8806f2e144da79e5ab53
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-10-09 23:02:25 +0900

    init: 어플리케이션 기본 실행 설정 파일 추가

---
+++ src/main/java/com/flab/bookfellas/BookFellasApplication.java
@@ -0,0 +1,13 @@
+package com.flab.bookfellas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookFellasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookFellasApplication.class, args);
+	}
+
+}
